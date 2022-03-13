b17b8f4b190be74d126509ae79e0712405043377
Artur Martini <artur.martini@bexsbanco.com.br>
2022-03-13 02:06:30 -0300

    centralize initialization in main file

--- main.go
+++ main.go
@@ -3,25 +3,31 @@ package main
 import (
 	"github.com/SmartTriageFiap/notification/config"
 	"github.com/SmartTriageFiap/notification/listener"
+	"github.com/SmartTriageFiap/notification/repository"
 	"github.com/SmartTriageFiap/notification/service"
 	"github.com/arturmartini/envconfig"
+	"github.com/segmentio/kafka-go"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 )
 
 func main() {
-	err := config.Configuration()
+	err := config.Initialize()
 	if err != nil {
 		log.WithError(err).Panic("error when try initialize config application")
 	}
-	listAddrs := strings.Split(envconfig.GetStr(config.KafkaAddr), " ")
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	hosts := strings.Split(envconfig.GetStr(config.KafkaAddr), " ")
 	topic := envconfig.GetStr(config.KafkaTopic)
-	svc := service.New()
+	reader := kafka.NewReader(kafka.ReaderConfig{Brokers: hosts, Topic: topic})
+	repo := repository.New()
+	svc := service.New(repo)
+	ltn := listener.New()
 
-	log.WithFields(log.Fields{
-		"kafka-addr":  listAddrs,
-		"kafka-topic": topic,
-	}).Info("getting settings")
-
-	log.WithError(listener.Start(svc, listAddrs, topic)).Panic("error when try start application")
+	log.WithError(ltn.Start(svc, reader, sigs)).Panic("error when try start application")
 }
