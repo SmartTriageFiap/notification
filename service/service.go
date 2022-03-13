7b260da973c7a95648ab09450394298c07e4543c
Artur Martini <artur.martini@bexsbanco.com.br>
2022-03-13 02:06:01 -0300

    adding logic to send notification and load partners

--- service/service.go
+++ service/service.go
@@ -1,6 +1,16 @@
 package service
 
-import "sync"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/SmartTriageFiap/notification/entities"
+	"github.com/SmartTriageFiap/notification/repository"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"sync"
+	"time"
+)
 
 var (
 	once     = sync.Once{}
@@ -8,20 +18,73 @@ var (
 )
 
 type Service interface {
-	Notify(string) error
+	Notify(event entities.Event) error
 }
 
-type svc struct{}
+type svc struct {
+	db       repository.Repository
+	mutex    sync.RWMutex
+	partners []entities.Partner
+	client   http.Client
+}
 
-func New() Service {
+func New(repo repository.Repository) Service {
 	if instance == nil {
 		once.Do(func() {
-			instance = &svc{}
+			svc := svc{
+				db: repo,
+				client: http.Client{
+					Timeout: time.Second * 10,
+				},
+			}
+
+			svc.gettingPartners()
+			instance = &svc
+
 		})
 	}
 	return instance
 }
 
-func (r svc) Notify(string) error {
+func (r svc) Notify(event entities.Event) error {
+	for _, partner := range r.partners {
+		b, err := json.Marshal(event)
+		if err != nil {
+			log.WithField("event", event).WithError(err).Warn("failed marshal event")
+			continue
+		}
+
+		req, err := http.NewRequest(http.MethodPost, partner.Url, bytes.NewReader(b))
+		if err != nil {
+			log.WithField("event", event).WithError(err).Warn("failed creating request")
+			continue
+		}
+
+		resp, err := r.client.Do(req)
+		if err != nil {
+			log.WithField("event", event).WithError(err).Warn("failed calling partner url")
+			continue
+		}
+
+		log.Info(fmt.Sprintf("event sent successfully url: %s status: %d", partner.Url, resp.Status))
+	}
+
 	return nil
+}
+
+func (r svc) gettingPartners() {
+	go func() {
+		for {
+			log.Info("getting partners")
+			partners, err := r.db.GetPartnersAddresses()
+			if err != nil {
+				log.WithError(err).Warn("failed getting partners")
+			}
+
+			r.mutex.RLock()
+			r.partners = partners
+			r.mutex.RUnlock()
+			<-time.After(time.Minute * 1)
+		}
+	}()
 }
